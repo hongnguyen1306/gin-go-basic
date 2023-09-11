9775128996ed28f10104747840b22c14400a1f50
Hong <hong@Hongs-iMac.local>
2023-09-11 08:52:13 +0700

    role user

--- modules/user/transport/ginuser/import_csv.go
+++ modules/user/transport/ginuser/import_csv.go
@@ -19,6 +19,12 @@ import (
 
 func HandleImportUserCsv(appCtx app_context.AppContext) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		requester := c.MustGet(common.CurrentUser).(common.Requester)
+		if requester.GetRole() != entity.SuperAdmin {
+			c.JSON(http.StatusForbidden, common.NewFailResponse("Bạn không có quyền thực hiện hành động này"))
+			return
+		}
+
 		file, header, _ := c.Request.FormFile("upload")
 
 		filename := header.Filename
